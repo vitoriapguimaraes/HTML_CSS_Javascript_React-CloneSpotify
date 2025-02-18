98646af1d666172388a20bc9d676a8baa5aa48df
Vitória Guimarães <87993956+vitoriapguimaraes@users.noreply.github.com>
2025-02-17 22:42:05 -0300

    ajustes no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,10 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL ="https://deploy-spotify-1mdb.onrender.com/api";
+
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
