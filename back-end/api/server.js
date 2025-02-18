032d2316b9187da2b93cda03f987d2117ced7f97
Vitória Guimarães <87993956+vitoriapguimaraes@users.noreply.github.com>
2025-02-17 22:26:59 -0300

    ajustes para deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -29,10 +29,10 @@ app.get("/api/songs", async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray());
 });
 
-app.use(express.static(path.join(__dirname, '../../front-end/dist')));
+app.use(express.static(path.join(__dirname, '../front-end/dist')));
 
 app.get("*", async (request, response) => {
-    response.sendFile(path.join(__dirname, '../../front-end/dist/index.html'));
+    response.sendFile(path.join(__dirname, '../front-end/dist/index.html'));
 });
 
 app.listen(PORT, () => {
